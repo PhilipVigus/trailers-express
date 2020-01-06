0cca82775aee9ab74574f70fbcc6598d4ae64778
PhilipVigus <philipvigus@yahoo.co.uk>
2020-01-06 15:52:45 +0000

    Create trailer database model

---
+++ models/trailer.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TrailerSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        guid: { type: String, required: true },
+        imageURL: { type: String, required: true },
+        articleDate: { type: Date, required: true },
+        trailerLink: { type: String, required: true },
+        tags: { type: String, required: true, default: "" },
+        rating: { type: Number, required: false, min: 1, max: 3 },
+        notes: { type: String, required: true, default: "" } 
+    }
+);
+
+module.exports = mongoose.model("Trailer", TrailerSchema);
