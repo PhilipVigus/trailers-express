ada0e5c22feaa5d9eb4d8f2fc84dc5371b7a20ca
PhilipVigus <philipvigus@yahoo.co.uk>
2020-01-06 16:24:07 +0000

    Asynchronously load test trailer documents into the database
    The async npm package was installed, as this is the easiest way
    to ensure that all of the loading is complete before the db
    connection closes itself.
    
    The 20 test trailers were loaded into the database.

--- data/thunderbirdDataFileReader.js
+++ data/thunderbirdDataFileReader.js
@@ -1,12 +1,30 @@
 'use strict';
 
+const mongoose = require("mongoose");
 const fileSystem = require("fs");
+const async = require("async");
 const TrailerModel = require("../models/trailer");
+const mongoDBConnectionString = "mongodb+srv://PortfolioAdmin:kj43Pipkj43@portfolio-databases-ndtpo.mongodb.net/trailers?retryWrites=true&w=majority";
 
-processFile("test-data.txt");
+mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true });
+const dbConnection = mongoose.connection;
 
-function processFile(filename) {
-    const data = readFileAsString(filename);
+// bind to the error event, so that errors get printed to console
+dbConnection.on("error", console.error.bind(console, 'MongoDB connection error:'));
+
+// this makes sure that the processing is complete before we close the database connection
+async.series(
+    [processFile],
+    function(err, results) {
+        if (err) {
+            console.log("FINAL ERR: " + err);
+        }
+        mongoose.connection.close();
+    }
+);
+
+function processFile() {
+    const data = readFileAsString("test-data.txt");
     const parsedData = parseData(data);
 }
 
@@ -33,11 +51,15 @@ function parseData(data) {
             imageURL: filmChunk.match(/<img[\s\S]*?>/)[0].slice(10, -4),
             articleDate: filmChunk.match(/Received.*/)[0].slice(24, -6),
             trailerLink: filmChunk.match(/<base[\s\S]*?>/)[0].slice(12, -2),
-            tags: filmChunk.match(/Tags[\s\S]*?<\/body>/)[0].replace(/<.*?>/g, "").slice(6, -4),
-            notes: ""
+            tags: filmChunk.match(/Tags[\s\S]*?<\/body>/)[0].replace(/<.*?>/g, "").slice(6, -4)
         };
 
         let trailer = new TrailerModel(trailerDetails);
+        trailer.save(function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
 
         // parse the title, removing the two tags with slice
         //film.title = filmChunk.match(/<title>[\s\S]*?<\/title>/)[0].slice(7, -8);
@@ -57,7 +79,7 @@ function parseData(data) {
         // parse the tags
         //film.tags = filmChunk.match(/Tags[\s\S]*?<\/body>/)[0].replace(/<.*?>/g, "").slice(6, -4);
 
-        films.push(trailer);
+        //films.push(trailer);
     })
 
     console.log(films);
--- models/trailer.js
+++ models/trailer.js
@@ -12,7 +12,7 @@ const TrailerSchema = new Schema(
         trailerLink: { type: String, required: true },
         tags: { type: String, required: true, default: "" },
         rating: { type: Number, required: false, min: 1, max: 3 },
-        notes: { type: String, required: true, default: "" } 
+        notes: { type: String, default: "" } 
     }
 );
 
