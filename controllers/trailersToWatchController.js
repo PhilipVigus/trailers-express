bac5472fa5ec40ef66b5f471e6fe7e22bf165da2
PhilipVigus <philipvigus@yahoo.co.uk>
2020-01-07 16:45:47 +0000

    Query DB for trailers with no ratings and pass into trailer view
    The query used to retrieve unrated trailers was updated and its
    results are now passed into the trailers to watch view. Currently
    only the trailer titles found are shown as proof of concept that
    everything links together properly.

--- controllers/trailersToWatchController.js
+++ controllers/trailersToWatchController.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const Trailer = require("../models/trailer");
+
 exports.trailerList = function (req, res) {
-    res.render("trailersToWatch", { title: "Trailers to Watch"});
+
+    // find all trailer documents that don't have a rating
+    Trailer.find({ rating: null }).exec(function (err, trailersFound) {
+        if (err) {
+            return next(err);
+        } else {
+            console.log(unescape(trailersFound[1].title));
+            res.render("trailersToWatch", { title: "Trailers to Watch", trailers_found: trailersFound })
+        }
+    });
 };
