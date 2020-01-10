2fd80dd893654df3616a2d288d723c2de1d87971
PhilipVigus <philipvigus@yahoo.co.uk>
2020-01-10 16:24:53 +0000

    Refactor code handling the rating star GUI interactions
    The JS code handling logic for interacting with the ratings stars
    and how information is stored/reset depending on what the user
    does with the dialog was reworked to make it simpler, and more
    logical separation into separate functions.

--- public/javascripts/trailersToWatch.js
+++ public/javascripts/trailersToWatch.js
@@ -1,8 +1,27 @@
 'use strict';
 
-function toggleRatingDialogVisibility(filmTitle="") {
+let currentRating = 0;
+let currentID = 0;
+
+function showRatingDialog(filmTitle, trailerID) {
+    currentID = trailerID;
     document.querySelector(".trailer-to-rate").textContent = filmTitle;
     document.querySelector(".modal-overlay").classList.toggle("show-modal");
+}
+
+function hideRatingDialog() {
+    currentID = 0;
+    currentRating = 0;
+    document.querySelector(".modal-overlay").classList.toggle("show-modal");
+    resetStars();
+}
+
+function resetStars() {
+    const starGroup = document.querySelectorAll(".star");
+
+    for (let i = 0; i < 3; i++) {
+        starGroup[i].classList.remove("star--lit");
+    }
 }
 
 function handleStarClick(eventTarget) {
@@ -18,6 +37,13 @@ function handleStarClick(eventTarget) {
         starGroup[1].classList.remove("star--lit");
         starGroup[2].classList.remove("star--lit");
 
+
+        if (starGroup[0].classList.contains("star--lit")) {
+            currentRating = 1;
+        } else {
+            currentRating = 0;
+        }
+
     } else if (eventTarget.id === "star-rating-2") {
         starGroup[0].classList.add("star--lit");
 
@@ -26,11 +52,29 @@ function handleStarClick(eventTarget) {
         }
 
         starGroup[2].classList.remove("star--lit");
+
+        if (starGroup[1].classList.contains("star--lit")) {
+            currentRating = 2;
+        } else {
+            currentRating = 1;
+        }
+
     } else {
 
         starGroup[0].classList.add("star--lit");
         starGroup[1].classList.add("star--lit");
         starGroup[2].classList.toggle("star--lit");
 
+        if (starGroup[2].classList.contains("star--lit")) {
+            currentRating = 3;
+        } else {
+            currentRating = 2;
+        }
+
     }
+}
+
+function sendRating() {
+    fetch(`/trailers-to-watch/rate?id=${currentID}&rating=${currentRating}`, { method: "PUT"});
+    hideRatingDialog();
 }
