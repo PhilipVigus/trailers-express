291307aa9b1214448b0d598c9b45be9acaac2fe3
PhilipVigus <philipvigus@yahoo.co.uk>
2020-01-13 17:18:12 +0000

    Create a placeholder shortlist view
    A simple shortlist view was created and wired into the router that
    displays the information from each film on the shortlist.

--- controllers/shortlistController.js
+++ controllers/shortlistController.js
@@ -3,5 +3,13 @@
 const Trailer = require("../models/trailer");
 
 exports.shortlist = function (req, res) {
-    res.send("shortlisty thingies");
+    Trailer.find({ rating: { $ne: null } }).exec(function (err, trailersFound) {
+        if (err) {
+            return next(err);
+        } else {
+            //res.send(`${trailersFound.length} trailers found`);
+            res.render("shortlist", { title: "Shortlist", trailers_found: trailersFound })
+        }
+    });
 };
+
